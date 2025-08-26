84f96c0cd7cb642285efb70a38c6f54066809d1d
Amanraj S <amanrajsathishkumar05@gmail.com>
2025-08-26 22:30:36 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -199,7 +199,7 @@ const Home = () => {
                 </div>
               </div>
                 <div className="bg-gray-100 rounded-lg shadow-md overflow-hidden flex flex-col">
-                <img src={acadsphere} alt="Weather App" className="w-full h-48 object-cover" />
+                <img src={acadsphere} alt="Acadsphere-(Students Grades Tracker)" className="w-full h-48 object-cover" />
                 <div className="p-4 flex flex-col items-center">
                   <h3 className="text-xl font-semibold mb-2">Todo-List App</h3>
                   <div className="flex space-x-4">
@@ -215,15 +215,15 @@ const Home = () => {
                 </div>
               </div>
                 <div className="bg-gray-100 rounded-lg shadow-md overflow-hidden flex flex-col">
-                <img src={task} alt="Weather App" className="w-full h-48 object-cover" />
+                <img src={task} alt="Tasklytic – tasks + analytics (reports, insights, tracking)" className="w-full h-48 object-cover" />
                 <div className="p-4 flex flex-col items-center">
                   <h3 className="text-xl font-semibold mb-2">Todo-List App</h3>
                   <div className="flex space-x-4">
-                    <a href="https://acadsphere.vercel.app/" target="_blank" rel="noopener noreferrer"
+                    <a href="https://task-manager-ten-gamma-53.vercel.app/login" target="_blank" rel="noopener noreferrer"
                       className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200">
                       Live Demo
                     </a>
-                    <a href="https://task-manager-ten-gamma-53.vercel.app/login" target="_blank" rel="noopener noreferrer"
+                    <a href="https://github.com/Amanraj-S/task-manager" target="_blank" rel="noopener noreferrer"
                       className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-900 transition duration-200">
                       Code
                     </a>
